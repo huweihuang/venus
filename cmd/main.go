214b05d3867e8b4ccac72285d8cd3c1be5ae4394
huweihuang <huweihuang@foxmail.com>
2023-11-11 18:55:32 +0800

    kubebuilder create api memcache

--- cmd/main.go
+++ cmd/main.go
@@ -96,6 +96,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Redis")
 		os.Exit(1)
 	}
+	if err = (&venuscontroller.MemcacheReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Memcache")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
